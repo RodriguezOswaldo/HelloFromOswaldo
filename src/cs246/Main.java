f495d5897e8f8a097a17b5abb43120e23f2e015c
Jose Albancando Robles <78940115+essunombre@users.noreply.github.com>
2021-10-07 20:17:09 -0500

    String friend added

--- src/cs246/Main.java
+++ src/cs246/Main.java
@@ -3,7 +3,9 @@ package cs246;
 public class Main {
 
     public static void main(String[] args) {
+        String friend = "Jose";
         Name name = new Name();
         name.printName();
+        System.out.printLn("Hi this is" + friend);
     }
 }
